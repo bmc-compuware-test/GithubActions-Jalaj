3b1ca1ca871c5e3f89ce546ee87524502b94b826
jarora8 <89238018+jarora8@users.noreply.github.com>
2024-04-25 13:42:50 +0530

    Update CWBWSUBC.cob

--- COB/CWBWSUBC.cob
+++ COB/CWBWSUBC.cob
@@ -5,7 +5,7 @@
       ******      C O M P U W A R E   C O R P O R A T I O N       ******
       *                                                                *
       *  THIS CALLED PROGRAM IS THE COBOL DEMO PROGRAM USED FOR        *
-      *  XPEDITER/TSO TRAINING SESSIONS. Change1                              *
+      *  XPEDITER/TSO TRAINING SESSIONS. JAlaj Change1                 *
       *                                                                *
       ******************************************************************
        ENVIRONMENT DIVISION.
