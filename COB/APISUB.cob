f8f400485feffbd8a3dd09ffcc3de33d9641a55e
jarora8 <89238018+jarora8@users.noreply.github.com>
2024-04-26 11:38:12 +0530

    Update APISUB.cob

--- COB/APISUB.cob
+++ COB/APISUB.cob
@@ -6,7 +6,7 @@
 000600
 000700********************************************************
 000800* THIS PROGRAM IS A TRAINING PROGRAM
-000900*      CALLED FROM    APITEST Change2
+000900*      CALLED FROM    APITEST Change1.1.1
 001000*      WITH COPYLIB   APICPYB (LINKAGE AREA)
 001100* ======================================================
 001200*  ISPW (TM)
